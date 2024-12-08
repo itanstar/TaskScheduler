a050e9d279f129a62e264acebf9dfa708256a150
sorocode <tudgus010707@naver.com>
2024-12-08 12:45:30 +0900

    🎨 logwriter 개선

--- 조재진/logWriter.c
+++ 조재진/logWriter.c
@@ -1,25 +1,30 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+// logWriter.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(int argc, char* argv[]){
-	if(argc != 3){
-		printf("Usage : ./logWriter <FileName> <Event>\n");
-		exit(0);
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage : ./logWriter <FileName> <Event>\n");
+		exit(EXIT_FAILURE);
 	}
- 	
-	FILE* file = fopen("log.txt", "a");
-	if(file == NULL){
-		printf("File Open Error\n");
-		exit(-1);
+
+	FILE *file = fopen("log.txt", "a");
+	if (file == NULL)
+	{
+		perror("File Open Error");
+		exit(EXIT_FAILURE);
 	}
 
 	time_t current_time = time(NULL);
-    struct tm *time_info = localtime(&current_time);
+	struct tm *time_info = localtime(&current_time);
 
-    char buffer[20];
-    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", time_info);
-    fprintf(file, "%s %s : %s\n", argv[1], argv[2], buffer);
+	char buffer[20];
+	strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", time_info);
+	fprintf(file, "%s %s : %s\n", argv[1], argv[2], buffer);
+	fclose(file);
 
-    return 0;
+	return 0;
 }
