401d05f7b82f467b5002bbc2b7ca22f53809a73a
sorocode <tudgus010707@naver.com>
2024-12-10 23:31:07 +0900

    🐛 UI 겹치는 문제 해결

--- src/ExecutorTest.c
+++ src/ExecutorTest.c
@@ -1,8 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]){
-	for(int i = 0; i < argc; i++){
-		printf("%s\n", argv[i]);
-	}
+#define MAX_LEN 512
+
+int main(int argc, char *argv[])
+{
+	char echo[MAX_LEN];
+
+	sprintf(echo, "%s %s; exec bash", argv[1], argv[2]);
+	execlp("gnome-terminal", "gnome-terminal", "--", "bash", "-c", echo, NULL);
+
 	return 0;
 }
