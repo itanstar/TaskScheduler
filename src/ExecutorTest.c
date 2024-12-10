17109558c34294f8d2009bd0a217abe5146f9f53
sorocode <tudgus010707@naver.com>
2024-12-10 23:45:55 +0900

    Revert "🐛 UI 겹치는 문제 해결"
    This reverts commit 401d05f7b82f467b5002bbc2b7ca22f53809a73a.

--- src/ExecutorTest.c
+++ src/ExecutorTest.c
@@ -1,15 +1,8 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+#include<stdio.h>
 
-#define MAX_LEN 512
-
-int main(int argc, char *argv[])
-{
-	char echo[MAX_LEN];
-
-	sprintf(echo, "%s %s; exec bash", argv[1], argv[2]);
-	execlp("gnome-terminal", "gnome-terminal", "--", "bash", "-c", echo, NULL);
-
+int main(int argc, char* argv[]){
+	for(int i = 0; i < argc; i++){
+		printf("%s\n", argv[i]);
+	}
 	return 0;
 }
